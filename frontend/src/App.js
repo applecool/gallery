0f14c10ebb53e07f0ef4112afd3f1e02ba973edf
applecool <kmitharsha@gmail.com>
2023-05-13 14:59:03 -0700

    Added header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
 function App() {
   return (
-    <div className="App">
-     <h1>Dummy app</h1>
+    <div>
+     <Header title='Photo gallery'></Header>
     </div>
   );
 }
