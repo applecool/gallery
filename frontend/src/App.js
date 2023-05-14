51eefaaa14d30547d3f1bf745b1f9dbfe3073b82
applecool <kmitharsha@gmail.com>
2023-05-13 19:30:03 -0700

    Hook unsplash api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,35 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
-import Search from './components/Search';
-import { useState } from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import { useState } from "react";
 
-function App() {
-  const [word, setWord] = useState('');
-  const handleSearchSubmit = (e) => {
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+
+const App = () => {
+  const [word, setWord] = useState("");
+  const handleSearchSubmit = async (e) => {
     e.preventDefault();
-    console.log(word);
-  }
+    const response = await fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    );
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      console.error(response.status);
+    }
+  };
+
   return (
     <div>
-     <Header title='Photo gallery'></Header>
-     <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}></Search>
+      <Header title="Photo gallery"></Header>
+      <Search
+        word={word}
+        setWord={setWord}
+        handleSubmit={handleSearchSubmit}
+      ></Search>
     </div>
   );
-}
+};
 
 export default App;
